4f9ec4ca02f464ae552c79bbe0cbaf84dfbe998f
carollingian <carolina.fdcampos@gmail.com>
2023-06-10 17:20:53 -0300

    teste 7

--- 8rainhas.cpp
+++ 8rainhas.cpp
@@ -19,10 +19,21 @@ int char1_count(const string str) {
   return qtde;
 }
 
+int not_char1or0(const string str) {
+  int qtde = 0;
+  for (char ch : str) {
+    if ((ch != '1') & (ch != '0') & (ch != '\n') &
+    (ch != '\0')) {
+        qtde++;
+      }
+ }
+  return qtde;
+} 
+
 int solucao_8rainhas(const string& teste) {
   string tabuleiro = conversorarquivotxt(teste);
   if (tabuleiro.empty() == true || tabuleiro.size() != 78 ||
-  char1_count(tabuleiro) != 8) {
+  char1_count(tabuleiro) != 8 || not_char1or0(tabuleiro) != 7) {
     return -1;
   } else {
     return char1_count(tabuleiro);
--- 8rainhas.hpp
+++ 8rainhas.hpp
@@ -6,4 +6,8 @@ using std::string;
 
 string conversorarquivotxt(const string& arq);
 
+int char1_count(const string str);
+
+int not_char1or0(const string str);
+
 int solucao_8rainhas(const string& teste);
