f2ff5a3fe72f8e633d8a8b12fbf69d89374a3a10
carollingian <carolina.fdcampos@gmail.com>
2023-06-11 20:35:26 -0300

    teste 25

--- html/menudata.js
+++ html/menudata.js
@@ -23,6 +23,6 @@
  @licend  The above is the entire license notice for the JavaScript code in this file
 */
 var menudata={children:[
-{text:"Main Page",url:"index.html"},
-{text:"Files",url:"files.html",children:[
-{text:"File List",url:"files.html"}]}]}
+{text:"Página Principal",url:"index.html"},
+{text:"Arquivos",url:"files.html",children:[
+{text:"Lista de Arquivos",url:"files.html"}]}]}
